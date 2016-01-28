fc28e802382da0d9105ba2c9b80c7075c1cc70b3
Aykin Cakaloz <aykin.cakaloz@ideasoft.com.tr>
2016-01-28 12:21:50 +0200

    Move Class

---
+++ Move.php
@@ -0,0 +1,50 @@
+<?php
+
+class Move
+{
+    /**
+     * @var int
+     */
+    private $x;
+    private $y;
+
+    public function __construct($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    /**
+     * @return int
+     */
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    /**
+     * @param int $x
+     */
+    public function setX($x)
+    {
+        $this->x = $x;
+    }
+
+    /**
+     * @return int
+     */
+    public function getY()
+    {
+        return $this->y;
+    }
+
+    /**
+     * @param int $y
+     */
+    public function setY($y)
+    {
+        $this->y = $y;
+    }
+
+}
+
--- run.php
+++ run.php
@@ -1,6 +1,10 @@
 #!/usr/bin/php
 <?php
 
+require_once "Game.php";
+require_once "Map.php";
+require_once "Move.php";
+
 // __main__ for some reason
 
 $game = new Game();
